0bedb3fb8b7297c3295c27af3995a9d174d9bb86
Joe Greiner <jrgreiner115@gmail.com>
2018-03-23 16:15:20 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -13,7 +13,7 @@ end
 
 def take_a_number(katz_deli, name)
 katz_deli << name
- 	 
-puts "Welcome, #{x}. You are number #{i+1} in line." if x == name	
+
+puts "Welcome, #{name}. You are number #{i+1} in line." if x == name	
 -  }	
  end	 end
