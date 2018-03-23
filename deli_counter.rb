a3c7750ff0eda0015806f2546d107f9b91cf6149
Joe Greiner <jrgreiner115@gmail.com>
2018-03-23 16:17:07 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,3 +16,9 @@ katz_deli << name
 
 puts "Welcome, #{name}. You are number #{katz_deli.count} in line."
 end
+
+def now_serving(katz_deli)
+  if katz_deli.count == 0
+puts "The line is empty!"
+else 
+  
